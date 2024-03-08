822c1b4ddfc47f1291ba20d6298045193dd0fe0a
SAVIO DUTRA <savio.dutra@mv.com.br>
2024-03-07 22:34:28 -0300

    correcao no momento de atualizacao da target durante uma modificação na lista de target

--- lib/app/modules/dashboard/page.dart
+++ lib/app/modules/dashboard/page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:target/app/data/models/target.dart';
+import 'package:intl/intl.dart';
+import 'package:target/app/data/services/auth/auth_service..dart';
 import 'package:target/app/modules/dashboard/commum.dart';
-import 'package:target/app/modules/dashboard/concluded.dart';
-import 'package:target/app/modules/dashboard/progress.dart';
 import 'package:target/app/modules/dashboard/controller.dart';
+import 'package:target/app/tools/functions.dart';
 import 'package:target/routes/routes.dart';
 
 class DashboardPage extends GetView<DashboardController> {
@@ -12,7 +12,6 @@ class DashboardPage extends GetView<DashboardController> {
 
   @override
   Widget build(BuildContext context) {
-
     //printd("width: $width");
 
     return MaterialApp(
@@ -71,13 +70,14 @@ class DashboardPage extends GetView<DashboardController> {
           ),
           floatingActionButton: Obx(() => btnDeposit(controller)),
           body: Obx(() {
-
             print("call page dashboard");
 
             return TabBarView(
               children: [
-                ProgressTarget(controller),
-                ConcludedTarget(controller),
+                //ProgressTarget(controller),
+                //ConcludedTarget(controller),
+                bodyDashboard(context, true),
+                bodyDashboard(context, false),
               ],
             );
           }),
@@ -85,5 +85,113 @@ class DashboardPage extends GetView<DashboardController> {
       ),
     );
   }
-}
 
+  Widget bodyDashboard(BuildContext context, bool inProgress) {
+    final double widthScreen = MediaQuery.of(context).size.width - 82;
+    final authService = Get.find<AuthService>();
+    final listaTarget = inProgress ? controller.progressTargets : controller.completeTargets;
+
+    printd(
+        "inProgress: $inProgress, quant: ${listaTarget.length}, isLoading: ${controller.loading.value}" +
+            ", isLogged: ${authService.isLogged}");
+
+    if (!authService.isLogged) {
+      return const Align(
+        alignment: Alignment.center,
+        child: Text("Por Favor, realizar login"),
+      );
+    }
+
+    if (controller.loading.value) {
+      return const Column(
+        children: [
+          Spacer(),
+          Center(
+            child: Text('carregando'),
+          ),
+          Spacer(),
+        ],
+      );
+    }
+
+    if (listaTarget.isEmpty) {
+      return Align(
+        alignment: Alignment.center,
+        child: Text(inProgress ? "Você não possui objetivos!" : "Você não possui objetivos concluidos!"),
+      );
+    }
+
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Text(
+              "Total Investido: ${NumberFormat.simpleCurrency().format(inProgress ? controller.sumOfAssets.value : controller.sumOfCompleted.value)}"),
+        ),
+        Expanded(
+          child: GridView.count(
+            primary: false,
+            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 75.0),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: List.generate(
+              listaTarget.length,
+              (index) {
+                return GestureDetector(
+                  onTap: () async {
+                    Map<String, dynamic> arg = {
+                      "target": listaTarget[index]
+                    };
+                    var edit = await Get.toNamed(
+                      Routes.item,
+                      arguments: arg,
+                    );
+                    printd("edit: $edit");
+                    if (edit != null && edit) controller.getAllTarget();
+                  },
+                  child: Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: SizedBox(
+                            height: (widthScreen / 2) - 45,
+                            width: (widthScreen / 2),
+                            child: returnImageFromString(
+                                listaTarget[index].imagem,
+                                widthScreen,
+                                const Icon(
+                                  Icons.local_mall,
+                                  size: 50,
+                                ),
+                                targetId: listaTarget[index].id),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "${listaTarget[index].posicao} - ${listaTarget[index].descricao}",
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              fittedBox(listaTarget[index]),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
